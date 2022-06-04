f68e2729aaf39ea47addb1d7d336c096fbe9f29e
Lucas Yamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2022-06-04 15:21:08 -0300

    Fix window constructor call

--- src/application.py
+++ src/application.py
@@ -1,9 +1,13 @@
 from window import Window
 
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 1000
+WINDOW_NAME = "Application"
+
 class Application:
     def __init__(self):
         """Initialize application"""
-        self.window = Window()
+        self.window = Window(WINDOW_NAME, WINDOW_WIDTH, WINDOW_HEIGHT)
         # Initialize application here
 
     def run(self):
