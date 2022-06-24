811501db4d590f0a6d0bc7f35354e8d308bf048d
lucasyamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2022-06-24 01:33:30 -0300

    Set x and z limits, and add exit key

--- src/input.py
+++ src/input.py
@@ -1,5 +1,6 @@
 import glm
 import math
+import glfw
 
 class Input:
     def __init__(self, camera, width, height):
@@ -41,17 +42,21 @@ class Input:
         # print("Mouse pos", xpos, ypos)
 
     def key_event(self, window, key, scancode, action, mods):
+        # Set exit key
+        if key == glfw.KEY_ESCAPE and (action==glfw.PRESS or action==glfw.REPEAT):
+            glfw.set_window_should_close(window, True)
+
         cameraSpeed = 1.0
-        if key == 87 and (action==1 or action==2): # tecla W
+        if key == glfw.KEY_W and (action==glfw.PRESS or action==glfw.REPEAT): # tecla W
             self.camera.pos += cameraSpeed * self.camera.front
         
-        if key == 83 and (action==1 or action==2): # tecla S
+        if key == glfw.KEY_S and (action==glfw.PRESS or action==glfw.REPEAT): # tecla S
             self.camera.pos -= cameraSpeed * self.camera.front
         
-        if key == 65 and (action==1 or action==2): # tecla A
+        if key == glfw.KEY_A and (action==glfw.PRESS or action==glfw.REPEAT): # tecla A
             self.camera.pos -= glm.normalize(glm.cross(self.camera.front, self.camera.up)) * cameraSpeed
             
-        if key == 68 and (action==1 or action==2): # tecla D
+        if key == glfw.KEY_D and (action==glfw.PRESS or action==glfw.REPEAT): # tecla D
             self.camera.pos += glm.normalize(glm.cross(self.camera.front, self.camera.up)) * cameraSpeed
 
         # print("Camera pos", self.camera.pos)
@@ -68,3 +73,13 @@ class Input:
         # Maximum height
         elif self.camera.pos.y > 50.0:
             self.camera.pos.y = 50.0
+
+        # Define x and z limits
+        if self.camera.pos.x < -120.0:
+            self.camera.pos.x = -120.0
+        elif self.camera.pos.x > 120.0:
+            self.camera.pos.x = 120.0
+        if self.camera.pos.z < -120.0:
+            self.camera.pos.z = -120.0
+        elif self.camera.pos.z > 120.0:
+            self.camera.pos.z = 120.0
